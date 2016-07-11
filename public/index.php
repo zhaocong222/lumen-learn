9b8f143ebc8eb4dd78e95059f645b49ee89ed9ff
zc <zhaocong@fdc.com.cn>
2016-07-11 18:02:57 +0800

    *

--- public/index.php
+++ public/index.php
@@ -12,17 +12,3 @@
 */
 
 $app = require __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$app->run();
